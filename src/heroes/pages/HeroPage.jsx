b3ea4060e03c1034de7e8c8e575dc8c99e592e61
Xanul <rivas.060@gmail.com>
2022-08-18 10:44:16 -0500

    Added the return button on HeroPage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,20 +1,49 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import { getHeroById } from '../helpers';
-
 
 
 export const HeroPage = () => {
 
   const { id } = useParams();
   const hero = getHeroById(id);
+  const navigate = useNavigate();
+  
+  const onNavigateBack = () => {
+    hero.publisher === 'Marvel Comics' ? navigate('/marvel') : navigate('/dc')
+  }
 
   if (!hero) {
     return <Navigate to="/marvel" />
   }
 
   return (
-    <>
-      <h1>{ hero.superhero }</h1>
-    </>
+    <div className='row mt-5'>
+      <div className="col-4">
+        <img 
+          src={`/assets/${hero.id}.jpg`} 
+          alt={ hero.superhero }
+          className='img-thumbnail'
+        />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter Ego:</b> {hero.alter_ego}</li>
+          <li className='list-group-item'><b>Publisher:</b> {hero.publisher}</li>
+          <li className='list-group-item'><b>First Appearance:</b> {hero.first_appearance}</li>
+        </ul>
+
+        <h5 className='mt-3'>Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button 
+          className='btn btn-outline-primary'
+          onClick={ onNavigateBack }
+        >
+          Regresar
+        </button>
+
+      </div>
+    </div>
   )
 }
