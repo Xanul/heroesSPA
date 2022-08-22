ab86dc3ceac18f2fea5c2b16a60b7c3f3f3f05f7
Xanul <rivas.060@gmail.com>
2022-08-22 15:58:42 -0500

    Added the local storage functionality

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,13 +3,11 @@ import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer";
 import { types } from "../types/types";
 
-const initialState = {
-  logged: false,
-}
 
 const init = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
 
+  const user = JSON.parse(localStorage.getItem('user'));
+  
   return {
     logged: !!user,
     user: user,
@@ -19,7 +17,7 @@ const init = () => {
 
 export const AuthProvider = ({ children }) => {
   
-  const [authState, dispatch] = useReducer(authReducer, initialState, init);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = async(name = '') => {
 
@@ -29,6 +27,7 @@ export const AuthProvider = ({ children }) => {
       type: types.login,
       payload: user
     }
+
     localStorage.setItem('user', JSON.stringify(user));
     dispatch(action);
 
