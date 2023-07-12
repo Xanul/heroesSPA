e98bd55352797e4636d3be41eb82bbe62239b074
Rodrigo Rivas <rivas.060@gmail.com>
2023-07-12 16:22:14 -0600

    Changed the navbar size

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-1">
       <Link className="navbar-brand" to="/">
         Asociaciones
       </Link>
